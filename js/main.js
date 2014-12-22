f3a6fbdc6b7fc08b9dbaf25c3891174934e17a6e
Robin Woodby <rlwoodby@gmail.com>
2014-12-22 14:31:28 -0800

    Added insta feed, formatted mobile a bit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+$(document).ready(function(){
 
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'flippant',
+        clientId: '49f5df8123e34a99985f415b635004f7',
+        template: '<a href="{{link}}"><img src="http://{{image}}" /></a>',
+        limit: 30
+    });
+    feed.run();
+
+});
